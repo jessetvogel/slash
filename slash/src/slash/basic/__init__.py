952332518199ec303c329f0922d51c97d86a35a2
Jesse Vogel <jesse.t.vogel@gmail.com>
2025-07-07 21:44:39 +0200

    add Pie to exports

--- slash/src/slash/basic/__init__.py
+++ slash/src/slash/basic/__init__.py
@@ -2,9 +2,10 @@
 
 from slash.basic.figure import Figure
 from slash.basic.markdown import Markdown
+from slash.basic.pie import Pie
 from slash.basic.progress import Progress
 from slash.basic.svg import SVG
 from slash.basic.table import Table
 from slash.basic.tabs import Tabs
 
-__all__: list[str] = ["Figure", "Markdown", "Progress", "Table", "Tabs", "SVG"]
+__all__: list[str] = ["Figure", "Markdown", "Progress", "Table", "Tabs", "SVG", "Pie"]
